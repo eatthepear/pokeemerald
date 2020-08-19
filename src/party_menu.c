d9ca68b96fa47a39e08deac34ac2feecb622375d
eatthepear <thelordofpear@gmail.com>
2020-08-19 02:24:59 -0400

    try for fix

--- src/party_menu.c
+++ src/party_menu.c
@@ -4926,7 +4926,8 @@ static void Task_PartyMenuReplaceMove(u8 taskId)
         RemoveMonPPBonus(mon, GetMoveSlotToReplace());
         move = gPartyMenu.data1;
         SetMonMoveSlot(mon, move, GetMoveSlotToReplace());
-        if ((GetMonData(mon, MON_DATA_PP1 + GetMoveSlotToReplace())) > arg){
+        u8 pp = (GetMonData(mon, MON_DATA_PP1 + GetMoveSlotToReplace()));
+        if (pp > arg){
         SetMonData(mon, MON_DATA_PP1 + GetMoveSlotToReplace(), arg);
         }
         Task_LearnedMove(taskId);
