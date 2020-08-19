e9ac2687d616870ecbc4ec216dba93a95040788a
eatthepear <thelordofpear@gmail.com>
2020-08-19 02:51:40 -0400

    pls

--- src/party_menu.c
+++ src/party_menu.c
@@ -4926,8 +4926,9 @@ static void Task_PartyMenuReplaceMove(u8 taskId)
         RemoveMonPPBonus(mon, GetMoveSlotToReplace());
         move = gPartyMenu.data1;
         SetMonMoveSlot(mon, move, GetMoveSlotToReplace());
-        
-        if (20 > arg){
+        arg[1] = GetMonData(mon, MON_DATA_PP1 + GetMoveSlotToReplace());
+        if (arg[1] > arg[0]){
+            arg[1] = 0;
             SetMonData(mon, MON_DATA_PP1 + GetMoveSlotToReplace(), arg);
         }
         Task_LearnedMove(taskId);
