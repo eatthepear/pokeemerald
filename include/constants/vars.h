7d6e15668fa5264de9c6f94c72970c8ad05a6a30
eatthepear <thelordofpear@gmail.com>
2020-08-27 01:48:06 -0400

    setting up VAR_ZONE for rewards after clearing each zone

--- include/constants/vars.h
+++ include/constants/vars.h
@@ -101,7 +101,7 @@
 #define VAR_HEAL_SHOP_USED                   0x4052 // custom var
 #define VAR_LAVARIDGE_TOWN_STATE             0x4053
 #define VAR_CURRENT_SECRET_BASE              0x4054 // was probably allocated for VAR_FALLARBOR_TOWN_STATE at one point
-#define VAR_VERDANTURF_TOWN_STATE            0x4055 // Unused Var
+#define VAR_ZONE                             0x4055 // custom Var
 #define VAR_PACIFIDLOG_TOWN_STATE            0x4056 // Unused Var
 #define VAR_PETALBURG_CITY_STATE             0x4057
 #define VAR_SLATEPORT_CITY_STATE             0x4058
