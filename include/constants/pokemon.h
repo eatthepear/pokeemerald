06a887b118db1f160b80f26cea89d4643b0a721a
eatthepear <thelordofpear@gmail.com>
2020-10-05 19:47:29 -0400

    removed friendship daynight and hold item day night

--- include/constants/pokemon.h
+++ include/constants/pokemon.h
@@ -296,35 +296,32 @@
 // Evolution type flags
 #define EVO_MEGA_EVOLUTION   0xffff // Not an actual evolution, used to temporarily mega evolve in battle.
 #define EVO_FRIENDSHIP       0x0001 // Pokémon levels up with friendship ≥ 220
-#define EVO_FRIENDSHIP_DAY   0x0002 // Pokémon levels up during the day with friendship ≥ 220
-#define EVO_FRIENDSHIP_NIGHT 0x0003 // Pokémon levels up at night with friendship ≥ 220
-#define EVO_LEVEL            0x0004 // Pokémon reaches the specified level
-#define EVO_TRADE            0x0005 // Pokémon is traded
-#define EVO_TRADE_ITEM       0x0006 // Pokémon is traded while it's holding the specified item
-#define EVO_ITEM             0x0007 // specified item is used on Pokémon
-#define EVO_LEVEL_ATK_GT_DEF 0x0008 // Pokémon reaches the specified level with attack > defense
-#define EVO_LEVEL_ATK_EQ_DEF 0x0009 // Pokémon reaches the specified level with attack = defense
-#define EVO_LEVEL_ATK_LT_DEF 0x000a // Pokémon reaches the specified level with attack < defense
-#define EVO_LEVEL_SILCOON    0x000b // Pokémon reaches the specified level with a Silcoon personality value
-#define EVO_LEVEL_CASCOON    0x000c // Pokémon reaches the specified level with a Cascoon personality value
-#define EVO_LEVEL_NINJASK    0x000d // Pokémon reaches the specified level (special value for Ninjask)
-#define EVO_LEVEL_SHEDINJA   0x000e // Pokémon reaches the specified level (special value for Shedinja)
-#define EVO_BEAUTY           0x000f // Pokémon levels up with beauty ≥ specified value
-#define EVO_LEVEL_FEMALE     0x0010 // Pokémon reaches the specified level, is female
-#define EVO_LEVEL_MALE       0x0011 // Pokémon reaches the specified level, is male
-#define EVO_LEVEL_NIGHT      0x0012 // Pokémon reaches the specified level, is night
-#define EVO_LEVEL_DAY        0x0013 // Pokémon reaches the specified level, is day
-#define EVO_LEVEL_DUSK       0x0014 // Pokémon reaches the specified level, is dusk (5-6 P.M)
-#define EVO_ITEM_HOLD_DAY    0x0015 // Pokémon levels up, holds specified item at day
-#define EVO_ITEM_HOLD_NIGHT  0x0016 // Pokémon levels up, holds specified item at night
-#define EVO_MOVE             0x0017 // Pokémon levels up, knows specified move
-#define EVO_MOVE_TYPE        0x0018 // Pokémon levels up, knows move with specified type
-#define EVO_MAP              0x0019 // Pokémon levels up on specified map
-#define EVO_ITEM_MALE        0x001A // specified item is used on a male Pokémon
-#define EVO_ITEM_FEMALE      0x001B // specified item is used on a female Pokémon
-#define EVO_LEVEL_RAIN       0x001C // Pokémon reaches the specified level while it's raining
-#define EVO_SPECIFIC_MON_IN_PARTY  0x001D // Pokémon levels up with a specified Pokémon in party
-#define EVO_LEVEL_DARK_TYPE_MON_IN_PARTY  0x001E // Pokémon reaches the specified level with a Dark Type Pokémon in party
+#define EVO_LEVEL            0x0002 // Pokémon reaches the specified level
+#define EVO_TRADE            0x0003 // Pokémon is traded
+#define EVO_TRADE_ITEM       0x0004 // Pokémon is traded while it's holding the specified item
+#define EVO_ITEM             0x0005 // specified item is used on Pokémon
+#define EVO_LEVEL_ATK_GT_DEF 0x0006 // Pokémon reaches the specified level with attack > defense
+#define EVO_LEVEL_ATK_EQ_DEF 0x0007 // Pokémon reaches the specified level with attack = defense
+#define EVO_LEVEL_ATK_LT_DEF 0x0008 // Pokémon reaches the specified level with attack < defense
+#define EVO_LEVEL_SILCOON    0x0009 // Pokémon reaches the specified level with a Silcoon personality value
+#define EVO_LEVEL_CASCOON    0x000a // Pokémon reaches the specified level with a Cascoon personality value
+#define EVO_LEVEL_NINJASK    0x000b // Pokémon reaches the specified level (special value for Ninjask)
+#define EVO_LEVEL_SHEDINJA   0x000c // Pokémon reaches the specified level (special value for Shedinja)
+#define EVO_BEAUTY           0x000d // Pokémon levels up with beauty ≥ specified value
+#define EVO_LEVEL_FEMALE     0x000e // Pokémon reaches the specified level, is female
+#define EVO_LEVEL_MALE       0x000f // Pokémon reaches the specified level, is male
+#define EVO_LEVEL_NIGHT      0x0010 // Pokémon reaches the specified level, is night
+#define EVO_LEVEL_DAY        0x0011 // Pokémon reaches the specified level, is day
+#define EVO_LEVEL_DUSK       0x0012 // Pokémon reaches the specified level, is dusk (5-6 P.M)
+#define EVO_ITEM_HOLD        0x0013 // Pokémon levels up, holds specified item at day
+#define EVO_MOVE             0x0014 // Pokémon levels up, knows specified move
+#define EVO_MOVE_TYPE        0x0015 // Pokémon levels up, knows move with specified type
+#define EVO_MAP              0x0016 // Pokémon levels up on specified map
+#define EVO_ITEM_MALE        0x0017 // specified item is used on a male Pokémon
+#define EVO_ITEM_FEMALE      0x0018 // specified item is used on a female Pokémon
+#define EVO_LEVEL_RAIN       0x0019 // Pokémon reaches the specified level while it's raining
+#define EVO_SPECIFIC_MON_IN_PARTY  0x001A // Pokémon levels up with a specified Pokémon in party
+#define EVO_LEVEL_DARK_TYPE_MON_IN_PARTY  0x001B // Pokémon reaches the specified level with a Dark Type Pokémon in party
 
 #define EVOS_PER_MON 8
 #define EVOS_PER_LINE 16
--- src/pokemon.c
+++ src/pokemon.c
@@ -5790,19 +5790,9 @@ u16 GetEvolutionTargetSpecies(struct Pokemon *mon, u8 type, u16 evolutionItem)
                 if (friendship >= 220)
                     targetSpecies = gEvolutionTable[species][i].targetSpecies;
                 break;
-            case EVO_FRIENDSHIP_DAY:
-                RtcCalcLocalTime();
-                if (gLocalTime.hours >= 12 && gLocalTime.hours < 24 && friendship >= 220)
-                    targetSpecies = gEvolutionTable[species][i].targetSpecies;
-                break;
             case EVO_LEVEL_DAY:
                 RtcCalcLocalTime();
                 if (gLocalTime.hours >= 12 && gLocalTime.hours < 24 && gEvolutionTable[species][i].param <= level)
-                    targetSpecies = gEvolutionTable[species][i].targetSpecies;
-                break;
-            case EVO_FRIENDSHIP_NIGHT:
-                RtcCalcLocalTime();
-                if (gLocalTime.hours >= 0 && gLocalTime.hours < 12 && friendship >= 220)
                     targetSpecies = gEvolutionTable[species][i].targetSpecies;
                 break;
             case EVO_LEVEL_NIGHT:
@@ -5810,16 +5800,7 @@ u16 GetEvolutionTargetSpecies(struct Pokemon *mon, u8 type, u16 evolutionItem)
                 if (gLocalTime.hours >= 0 && gLocalTime.hours < 12 && gEvolutionTable[species][i].param <= level)
                     targetSpecies = gEvolutionTable[species][i].targetSpecies;
                 break;
-            case EVO_ITEM_HOLD_NIGHT:
-                RtcCalcLocalTime();
-                if (gLocalTime.hours >= 0 && gLocalTime.hours < 12 && heldItem == gEvolutionTable[species][i].param)
-                {
-                    heldItem = 0;
-                    SetMonData(mon, MON_DATA_HELD_ITEM, &heldItem);
-                    targetSpecies = gEvolutionTable[species][i].targetSpecies;
-                }
-                break;
-            case EVO_ITEM_HOLD_DAY:
+            case EVO_ITEM_HOLD:
                 RtcCalcLocalTime();
                 if (gLocalTime.hours >= 12 && gLocalTime.hours < 24 && heldItem == gEvolutionTable[species][i].param)
                 {
