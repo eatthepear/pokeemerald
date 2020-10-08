6b4d8d1816acf30f4a351b7ca196fcecbb5ecdd3
eatthepear <thelordofpear@gmail.com>
2020-10-08 14:26:07 -0400

    fix errors

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -862,7 +862,7 @@ static const u16 sRarePickupItems[] =
     ITEM_WHITE_HERB,
     ITEM_TM44_REST,
     ITEM_ELIXIR,
-    ITEM_TM01_RETURN,
+    ITEM_TM01_PROTECT,
     ITEM_LEFTOVERS,
     ITEM_TM26_EARTHQUAKE,
 };
--- src/data/pokemon/tmhm_learnsets.h
+++ src/data/pokemon/tmhm_learnsets.h
@@ -1,5 +1,5 @@
 #define TMHM_LEARNSET(moves) {(u32)(moves), ((u64)(moves) >> 32)}
-#define TMHM(tmhm) ((u64)1 << (ITEM_##tmhm - ITEM_TM01_RETURN))
+#define TMHM(tmhm) ((u64)1 << (ITEM_##tmhm - ITEM_TM01_PROTECT))
 
 // This table determines which TMs and HMs a species is capable of learning.
 // Each entry is a 64-bit bit array spread across two 32-bit values, with
