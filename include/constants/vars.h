8327007a10d2560aa812711606b57e37a0602c75
eatthepear <thelordofpear@gmail.com>
2020-10-18 11:25:47 -0400

    edited the advice function

--- include/constants/vars.h
+++ include/constants/vars.h
@@ -102,10 +102,10 @@
 #define VAR_LAVARIDGE_TOWN_STATE             0x4053
 #define VAR_CURRENT_SECRET_BASE              0x4054 // was probably allocated for VAR_FALLARBOR_TOWN_STATE at one point
 #define VAR_ZONE                             0x4055 // custom Var
-#define VAR_ZHULI_INTRO                      0x4056 // Unused Var
+#define VAR_ZHULI_INTRO                      0x4056 // ZhuLiIntro
 #define VAR_PETALBURG_CITY_STATE             0x4057
 #define VAR_SLATEPORT_CITY_STATE             0x4058
-#define VAR_MAUVILLE_CITY_STATE              0x4059 // Unused Var
+#define VAR_ADVICE                           0x4059 // Cycle through advice
 #define VAR_RUSTBORO_CITY_STATE              0x405A
 #define VAR_FORTREE_CITY_STATE               0x405B // Unused Var
 #define VAR_LILYCOVE_CITY_STATE              0x405C // Unused Var
