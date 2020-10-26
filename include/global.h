5dadc605815cf9dcb877f06c6b163315d93aec64
eatthepear <thelordofpear@gmail.com>
2020-10-25 23:01:32 -0400

    added back in rounding value Q_4_12_ROUND

--- include/global.h
+++ include/global.h
@@ -68,6 +68,10 @@
 // Converts a Q24.8 fixed-point format number to a regular integer
 #define Q_24_8_TO_INT(n) ((int)((n) >> 8))
 
+// Rounding value for Q4.12 fixed-point format
+#define Q_4_12_ROUND ((1) << (12 - 1))
+#define UQ_4_12_ROUND ((1) << (12 - 1))
+
 #define min(a, b) ((a) < (b) ? (a) : (b))
 #define max(a, b) ((a) >= (b) ? (a) : (b))
 
