0fb1fd27029bc3efffe355559772b1e825fcc3aa
eatthepear <thelordofpear@gmail.com>
2021-07-11 22:23:40 -0400

    preliminary zone21 map

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1236,3 +1236,5 @@ Common_EventScript_LegendaryFlewAway:: @ 8273776
 	.include "data/maps/Zone20Sanctuary/scripts.inc"
 
 	.include "data/maps/Zone20Shops/scripts.inc"
+
+	.include "data/maps/Zone21A/scripts.inc"
