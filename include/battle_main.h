bb603f749748d799cb61e7ec7bc76a4b1c0a2ea9
eatthepear <thelordofpear@gmail.com>
2022-01-06 12:26:01 -0500

    added class based pokeballs

--- include/battle_main.h
+++ include/battle_main.h
@@ -22,6 +22,12 @@ struct MultiPartnerMenuPokemon
     /*0x1D*/ u8 language;
 };
 
+struct TrainerBall
+{
+    u8 classId;
+    u8 Ball; // make this a u16 if needed
+};
+
 #define TYPE_NAME_LENGTH 6
 
 #if B_EXPANDED_ABILITY_NAMES
