d06f4f6696a7448b9840e5e1ccffab4724845a53
eatthepear <thelordofpear@gmail.com>
2022-01-15 12:45:13 -0500

    testing title screen music

--- src/title_screen.c
+++ src/title_screen.c
@@ -611,7 +611,7 @@ void CB2_InitTitleScreen(void)
                                     | DISPCNT_OBJ_ON
                                     | DISPCNT_WIN0_ON
                                     | DISPCNT_OBJWIN_ON);
-        m4aSongNumStart(MUS_TITLE);
+        m4aSongNumStart(MUS_BW_SEQ_BGM_POKEMON_THEME);
         gMain.state = 5;
         break;
     case 5:
