9d117f16b536cbedcefe3f536fb47ac4e6fe0e75
eatthepear <thelordofpear@gmail.com>
2022-05-07 02:04:46 -0400

    heal player party no longer heals fainted pokemon

--- src/script_pokemon_util.c
+++ src/script_pokemon_util.c
@@ -38,6 +38,11 @@ void HealPlayerParty(void)
     for(i = 0; i < gPlayerPartyCount; i++)
     {
         u16 maxHP = GetMonData(&gPlayerParty[i], MON_DATA_MAX_HP);
+        u16 curHP = GetMonData(&gPlayerParty[i], MON_DATA_HP);
+        if (NuzlockeFlagGet(GLOBAL_NUZLOCKE_SWITCH) && curHP == 0) {
+            continue;
+        }
+        
         arg[0] = maxHP;
         arg[1] = maxHP >> 8;
         SetMonData(&gPlayerParty[i], MON_DATA_HP, arg);
@@ -58,42 +63,6 @@ void HealPlayerParty(void)
         SetMonData(&gPlayerParty[i], MON_DATA_STATUS, arg);
     }
 }
-
-/*
- void HealPlayerParty(void)
- {
- u8 i, j;
- u8 ppBonuses;
- u8 arg[4];
- 
- // restore HP.
- for(i = 0; i < gPlayerPartyCount; i++)
- {
- if (((!(GetMonData(&gPlayerParty[i], MON_DATA_HP) == 0)) && NuzlockeFlagGet(GLOBAL_NUZLOCKE_SWITCH)) || (!(NuzlockeFlagGet(GLOBAL_NUZLOCKE_SWITCH))))
- {
- u16 maxHP = GetMonData(&gPlayerParty[i], MON_DATA_MAX_HP);
- arg[0] = maxHP;
- arg[1] = maxHP >> 8;
- SetMonData(&gPlayerParty[i], MON_DATA_HP, arg);
- ppBonuses = GetMonData(&gPlayerParty[i], MON_DATA_PP_BONUSES);
- 
- // restore PP.
- for(j = 0; j < MAX_MON_MOVES; j++)
- {
- arg[0] = CalculatePPWithBonus(GetMonData(&gPlayerParty[i], MON_DATA_MOVE1 + j), ppBonuses, j);
- SetMonData(&gPlayerParty[i], MON_DATA_PP1 + j, arg);
- }
- 
- // since status is u32, the four 0 assignments here are probably for safety to prevent undefined data from reaching SetMonData.
- arg[0] = 0;
- arg[1] = 0;
- arg[2] = 0;
- arg[3] = 0;
- SetMonData(&gPlayerParty[i], MON_DATA_STATUS, arg);
- }
- }
- }
- */
 
 u8 ScriptGiveMon(u16 species, u8 level, u16 item, u32 unused1, u32 unused2, u8 unused3)
 {
