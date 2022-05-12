d7540571183ea29349f2ebc02a6f6de28e2edbdb
eatthepear <thelordofpear@gmail.com>
2022-05-12 12:36:19 -0400

    fixed some errors

--- include/constants/battle_config.h
+++ include/constants/battle_config.h
@@ -21,7 +21,7 @@
 
 // Exp and stat settings
 #define B_EXP_CATCH                 GEN_4 // In Gen6+, Pokémon get experience from catching.
-#define B_TRAINER_EXP_MULTIPLIER    GEN_4 // In Gen7+, trainer battles no longer give a 1.5 multiplier to EXP gain.
+#define B_TRAINER_EXP_MULTIPLIER    GEN_8 // In Gen7+, trainer battles no longer give a 1.5 multiplier to EXP gain.
 #define B_SPLIT_EXP                 GEN_4 // In Gen6+, all participating mon get full experience.
 #define B_SCALED_EXP                GEN_4 // In Gen5 and Gen7+, experience is weighted by level difference.
 #define B_BADGE_BOOST               GEN_7 // In Gen4+, Gym Badges no longer boost a Pokémon's stats.
