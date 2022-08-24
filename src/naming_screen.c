8bfd0a7538fbecbe65c31fffcad37510047c2e3f
eatthepear <thelordofpear@gmail.com>
2022-08-24 15:47:27 -0400

    added naming screen change case with r button

--- src/naming_screen.c
+++ src/naming_screen.c
@@ -37,7 +37,8 @@ enum {
     INPUT_DPAD_RIGHT,
     INPUT_A_BUTTON,
     INPUT_B_BUTTON,
-    INPUT_LR_BUTTON,
+    INPUT_L_BUTTON,
+    INPUT_R_BUTTON,
     INPUT_SELECT,
     INPUT_START,
 };
@@ -385,6 +386,7 @@ static u8 GetTextEntryPosition(void);
 static void DeleteTextCharacter(void);
 static bool8 AddTextCharacter(void);
 static void BufferCharacter(u8);
+static void ChangeCharacterCase(void);
 static void SaveInputText(void);
 static void LoadGfx(void);
 static void CreateHelperTasks(void);
@@ -1476,6 +1478,11 @@ static bool8 HandleKeyboardEvent(void)
         DeleteTextCharacter();
         return FALSE;
     }
+    else if (input == INPUT_R_BUTTON)
+    {
+        ChangeCharacterCase();
+        return FALSE;
+    }
     else if (input == INPUT_START)
     {
         MoveCursorToOKButton();
@@ -1603,6 +1610,8 @@ static void Input_Enabled(struct Task *task)
         task->tKeyboardEvent = INPUT_A_BUTTON;
     else if (JOY_NEW(B_BUTTON))
         task->tKeyboardEvent = INPUT_B_BUTTON;
+    else if (JOY_NEW(R_BUTTON))
+        task->tKeyboardEvent = INPUT_R_BUTTON;
     else if (JOY_NEW(SELECT_BUTTON))
         task->tKeyboardEvent = INPUT_SELECT;
     else if (JOY_NEW(START_BUTTON))
@@ -2582,4 +2591,24 @@ static const struct SpritePalette sSpritePalettes[] =
     {}
 };
 
+static void ChangeCharacterCase(void)
+{
+    u8 index = GetPreviousTextCaretPosition();
+
+    if (sNamingScreen->textBuffer[index] >= CHAR_A && sNamingScreen->textBuffer[index] <= CHAR_Z)
+    {
+        sNamingScreen->textBuffer[index] = sNamingScreen->textBuffer[index] + 0x1A;
+    }
+    else if (sNamingScreen->textBuffer[index] >= CHAR_a && sNamingScreen->textBuffer[index] <= CHAR_z)
+    {
+        sNamingScreen->textBuffer[index] = sNamingScreen->textBuffer[index] - 0x1A;
+    }
+    else
+    {
+        sNamingScreen->textBuffer[index] = sNamingScreen->textBuffer[index];
+    }
+    DrawTextEntry();
+    CopyBgTilemapBufferToVram(3);
+    PlaySE(SE_SELECT);
+}
 
