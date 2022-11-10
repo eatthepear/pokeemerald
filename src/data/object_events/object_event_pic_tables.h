c4ebac89c9961ff35d432f5ebcb9818276a65542
eatthepear <thelordofpear@gmail.com>
2022-11-10 05:41:12 -0500

    animated porygon

--- src/data/object_events/object_event_pic_tables.h
+++ src/data/object_events/object_event_pic_tables.h
@@ -1935,24 +1935,24 @@ static const struct SpriteFrameImage sPicTable_Porygon[] = {
     overworld_frame(gObjectEventPic_Porygon, 4, 4, 0),
     overworld_frame(gObjectEventPic_Porygon, 4, 4, 1),
     overworld_frame(gObjectEventPic_Porygon, 4, 4, 2),
-    overworld_frame(gObjectEventPic_Porygon, 4, 4, 0),
-    overworld_frame(gObjectEventPic_Porygon, 4, 4, 0),
-    overworld_frame(gObjectEventPic_Porygon, 4, 4, 1),
-    overworld_frame(gObjectEventPic_Porygon, 4, 4, 1),
-    overworld_frame(gObjectEventPic_Porygon, 4, 4, 2),
-    overworld_frame(gObjectEventPic_Porygon, 4, 4, 2),
+    overworld_frame(gObjectEventPic_Porygon, 4, 4, 3),
+    overworld_frame(gObjectEventPic_Porygon, 4, 4, 4),
+    overworld_frame(gObjectEventPic_Porygon, 4, 4, 5),
+    overworld_frame(gObjectEventPic_Porygon, 4, 4, 6),
+    overworld_frame(gObjectEventPic_Porygon, 4, 4, 7),
+    overworld_frame(gObjectEventPic_Porygon, 4, 4, 8),
 };
 
 static const struct SpriteFrameImage sPicTable_ShinyPorygon[] = {
     overworld_frame(gObjectEventPic_ShinyPorygon, 4, 4, 0),
     overworld_frame(gObjectEventPic_ShinyPorygon, 4, 4, 1),
     overworld_frame(gObjectEventPic_ShinyPorygon, 4, 4, 2),
-    overworld_frame(gObjectEventPic_ShinyPorygon, 4, 4, 0),
-    overworld_frame(gObjectEventPic_ShinyPorygon, 4, 4, 0),
-    overworld_frame(gObjectEventPic_ShinyPorygon, 4, 4, 1),
-    overworld_frame(gObjectEventPic_ShinyPorygon, 4, 4, 1),
-    overworld_frame(gObjectEventPic_ShinyPorygon, 4, 4, 2),
-    overworld_frame(gObjectEventPic_ShinyPorygon, 4, 4, 2),
+    overworld_frame(gObjectEventPic_ShinyPorygon, 4, 4, 3),
+    overworld_frame(gObjectEventPic_ShinyPorygon, 4, 4, 4),
+    overworld_frame(gObjectEventPic_ShinyPorygon, 4, 4, 5),
+    overworld_frame(gObjectEventPic_ShinyPorygon, 4, 4, 6),
+    overworld_frame(gObjectEventPic_ShinyPorygon, 4, 4, 7),
+    overworld_frame(gObjectEventPic_ShinyPorygon, 4, 4, 8),
 };
 
 static const struct SpriteFrameImage sPicTable_Wingull[] = {
