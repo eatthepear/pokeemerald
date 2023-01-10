47fb77679ff7005ab8e38ce8f6edfef2ad2804a0
eatthepear <thelordofpear@gmail.com>
2023-01-09 18:57:29 -0800

    added back in field poison and made survive at 1 hp

--- src/field_poison.c
+++ src/field_poison.c
@@ -45,7 +45,7 @@ static void FaintFromFieldPoison(u8 partyIdx)
     struct Pokemon *pokemon = &gPlayerParty[partyIdx];
     u32 status = STATUS1_NONE;
 
-    AdjustFriendship(pokemon, FRIENDSHIP_EVENT_FAINT_FIELD_PSN);
+    // AdjustFriendship(pokemon, FRIENDSHIP_EVENT_FAINT_FIELD_PSN);
     SetMonData(pokemon, MON_DATA_STATUS, &status);
     GetMonData(pokemon, MON_DATA_NICKNAME, gStringVar1);
     StringGet_Nickname(gStringVar1);
@@ -54,7 +54,7 @@ static void FaintFromFieldPoison(u8 partyIdx)
 static bool32 MonFaintedFromPoison(u8 partyIdx)
 {
     struct Pokemon *pokemon = &gPlayerParty[partyIdx];
-    if (IsMonValidSpecies(pokemon) && GetMonData(pokemon, MON_DATA_HP) == 0 && GetAilmentFromStatus(GetMonData(pokemon, MON_DATA_STATUS)) == AILMENT_PSN)
+    if (IsMonValidSpecies(pokemon) && GetMonData(pokemon, MON_DATA_HP) == 1 && GetAilmentFromStatus(GetMonData(pokemon, MON_DATA_STATUS)) == AILMENT_PSN)
         return TRUE;
 
     return FALSE;
@@ -116,39 +116,39 @@ void TryFieldPoisonWhiteOut(void)
 
 s32 DoPoisonFieldEffect(void)
 {
-    // int i;
-    // u32 hp;
-    // struct Pokemon *pokemon = gPlayerParty;
-    // u32 numPoisoned = 0;
-    // u32 numFainted = 0;
+    int i;
+    u32 hp;
+    struct Pokemon *pokemon = gPlayerParty;
+    u32 numPoisoned = 0;
+    u32 numFainted = 0;
 
-    // for (i = 0; i < PARTY_SIZE; i++)
-    // {
-    //     if (GetMonData(pokemon, MON_DATA_SANITY_HAS_SPECIES) && GetAilmentFromStatus(GetMonData(pokemon, MON_DATA_STATUS)) == AILMENT_PSN
-    //        && GetMonAbility(pokemon) != ABILITY_POISON_HEAL
-    //        && GetMonAbility(pokemon) != ABILITY_MAGIC_GUARD
-    //        && GetMonAbility(pokemon) != ABILITY_IMMUNITY)
-    //     {
-    //         // Apply poison damage
-    //         hp = GetMonData(pokemon, MON_DATA_HP);
-    //         if (hp == 0 || --hp == 0)
-    //             numFainted++;
+    for (i = 0; i < PARTY_SIZE; i++)
+    {
+        if (GetMonData(pokemon, MON_DATA_SANITY_HAS_SPECIES) && GetAilmentFromStatus(GetMonData(pokemon, MON_DATA_STATUS)) == AILMENT_PSN
+           && GetMonAbility(pokemon) != ABILITY_POISON_HEAL
+           && GetMonAbility(pokemon) != ABILITY_MAGIC_GUARD
+           && GetMonAbility(pokemon) != ABILITY_IMMUNITY)
+        {
+            // Apply poison damage
+            hp = GetMonData(pokemon, MON_DATA_HP);
+            if (hp == 1 || --hp == 1)
+                numFainted++;
 
-    //         SetMonData(pokemon, MON_DATA_HP, &hp);
-    //         numPoisoned++;
-    //     }
-    //     pokemon++;
-    // }
+            SetMonData(pokemon, MON_DATA_HP, &hp);
+            numPoisoned++;
+        }
+        pokemon++;
+    }
 
-    // // Do screen flash effect
-    // if (numFainted != 0 || numPoisoned != 0)
-    //     FldEffPoison_Start();
+    // Do screen flash effect
+    if (numFainted != 0 || numPoisoned != 0)
+        FldEffPoison_Start();
 
-    // if (numFainted != 0)
-    //     return FLDPSN_FNT;
+    if (numFainted != 0)
+        return FLDPSN_FNT;
 
-    // if (numPoisoned != 0)
-    //     return FLDPSN_PSN;
+    if (numPoisoned != 0)
+        return FLDPSN_PSN;
 
     return FLDPSN_NONE;
 }
